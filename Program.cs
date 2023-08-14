4d2fc5002ea57f3aadb7569652b90cc97e9b08e8
Douglas Sánchez <142137936+DouglasSanchezMarchelli@users.noreply.github.com>
2023-08-14 14:31:33 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace LeerArchivoTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leyendo Contenido de Archivo de Texto");
+            Console.WriteLine("====================================");
+
+            Console.Write("Ingrese la ruta completa del archivo de texto (ejemplo: C:\\ruta\\archivo.txt): ");
+            string rutaArchivo = Console.ReadLine();
+
+            if (File.Exists(rutaArchivo))
+            {
+                try
+                {
+                    string contenido = File.ReadAllText(rutaArchivo);
+                    Console.WriteLine("Contenido del Archivo:");
+                    Console.WriteLine("=======================");
+                    Console.WriteLine(contenido);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error al leer el archivo: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El archivo no existe.");
+            }
+        }
+    }
+}
